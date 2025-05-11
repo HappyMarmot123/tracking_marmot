9abe0ce0372ec773cd948c1da785ba4027f43731
HappyMarmot123 <mdnsw28@gmail.com>
2025-05-11 21:45:16 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# tracking_marmot
+
+기술스택  
+Flutter
+DrizzleORM  
+Supabase  
+Shorebird
+
+flutter run
---
+++ android/app/src/main/kotlin/com/example/tracking_marmot/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tracking_marmot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
